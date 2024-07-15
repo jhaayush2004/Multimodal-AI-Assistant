e9431dc32a59d9f136a0a30988ffed9a6867fbc9
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2024-07-15 11:22:19 +0530

    Create setup.py
    At backend requirements.txt will call setup.py as meta data of this local package where __init__.py is available.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(name="GenAI Project",
+      version="0.0.0",
+      author="Ayush Shaurya Jha",
+      author_email="shauryasphinx@gmail.com",
+      description="A project to explore and implement various AI concepts",
+      url="https://github.com/jhaayush2004",
+      packages=find_packages()
+)
