ea0f0c96af6dcdcf16dcdb9aacb46de733e5892b
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2024-07-15 11:31:34 +0530

    Create handlers_imports.py

---
+++ handlers_imports.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+from gtts import gTTS
+import PIL.Image
+import time
+from markdown import Markdown
+import streamlit as st
